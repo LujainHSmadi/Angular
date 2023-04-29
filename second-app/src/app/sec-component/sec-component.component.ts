a172466a125417906d4cfa7cc6dd09824a502bae
LujainHSmadi <lujainhsmadi@gmail.com>
2023-04-29 16:41:10 +0300

    lujain 29-4-2023 04:15 PM

--- second-app/src/app/sec-component/sec-component.component.ts
+++ second-app/src/app/sec-component/sec-component.component.ts
@@ -13,16 +13,50 @@ export class SecComponentComponent {
     imageURL:'https://cdn.dribbble.com/users/1397859/screenshots/15623557/98_4x.jpg'
   }
 
-  myName = ''
+  myName = '';
   change(){
-    this.user.name = 'Malak Smadi',
+    this.user.name = this.myName,
     this.user.age = 21,
     this.user.imageURL ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU40ifYM7jFVe5KIkDz2bOqYM_AaWnrWhqqw&usqp=CAU"
   }
-  change2(){
-    this.user.name = 'Lujain Smadi',
-    this.user.age = 29,
-    this.user.imageURL='https://cdn.dribbble.com/users/1397859/screenshots/15623557/98_4x.jpg'
 
-  }
+  color = 'yellow';
+ 
+car = [
+  'BMW',
+  'Toyota',
+  'Rang Rover'
+];
+
+students = [
+  {
+  name:'ali',
+  age:22,
+  bg: 'yellow',
+  color:'blue'
+},
+  {
+  name:'sami',
+  age:28,
+  bg: 'red',
+  color:'yellow'
+
+},
+  {
+  name:'serine',
+  age:21,
+  bg:'green',
+  color:'red'
+},
+  {
+  name:'omar',
+  age:4,
+  bg:'yellow',
+color:"black"
+}
+
+]
+
+  display = true;
+
 }
